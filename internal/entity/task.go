89b96f78480e97855b7c7eb987de043a07584c4d
Marat Khasbiullin <marat.xasbiullin@gmail.com>
2024-06-10 12:07:10 +0300

    init

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -10,14 +10,14 @@ type TaskResponse struct {
 	ID          int       `json:"id" example:"22"`
 	Title       string    `json:"title" example:"Read a book"`
 	Description string    `json:"description" example:"Read the book The Lord of the Rings, stopped at the third chapter"`
-	DueDate     time.Time `json:"due_date" example:"2024-06-09"`
+	DueDate     time.Time `json:"due_date" example:"2024-06-09T00:00:00Z"`
 	Completed   bool      `json:"completed" example:"true"`
 }
 
 type TaskRequest struct {
 	Title       string    `json:"title" example:"Read a book"`
 	Description string    `json:"description" example:"Read the book The Lord of the Rings, stopped at the third chapter"`
-	DueDate     time.Time `json:"due_date" example:"2024-06-09T00:00:00Z"`
+	DueDate     time.Time `json:"due_date" example:"2024-06-09"`
 	Completed   bool      `json:"completed" example:"true"`
 }
 
